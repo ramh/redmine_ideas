36ca99255bd99a70e801985bd222be2192953d69
Ram Kumar Hariharan <coolramkumar@gmail.com>
2009-06-24 23:05:53 +0530

    Initiating the Redmine Ideas Board Plugin. Template created using generate redmine plugin command

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_ideas do
+  name 'Redmine Ideas plugin'
+  author 'Ram Kumar Hariharan'
+  description 'A plugin for managing and voting on ideas for projects'
+  version '0.0.1'
+end
